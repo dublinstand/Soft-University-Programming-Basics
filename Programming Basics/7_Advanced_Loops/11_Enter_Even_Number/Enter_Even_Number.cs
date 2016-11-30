68e9deb3edbf86e5b05fe2baf8ddeb31d5750873
Deyan <dublinstand@gmail.com>
2016-11-30 08:30:31 +0000

    modified:   Programming Basics/7_Advanced_Loops/11_Enter_Even_Number/11_Enter_Even_Number.csproj 	modified:   Programming Basics/7_Advanced_Loops/11_Enter_Even_Number/Enter_Even_Number.cs 	new file:   Programming Basics/7_Advanced_Loops/12_Fibonacci/12_Fibonacci.csproj 	new file:   Programming Basics/7_Advanced_Loops/12_Fibonacci/App.config 	new file:   Programming Basics/7_Advanced_Loops/12_Fibonacci/Fibonacci.cs 	new file:   Programming Basics/7_Advanced_Loops/12_Fibonacci/Properties/AssemblyInfo.cs 	new file:   Programming Basics/7_Advanced_Loops/13_Number_Pyramid/13_Number_Pyramid.csproj 	new file:   Programming Basics/7_Advanced_Loops/13_Number_Pyramid/App.config 	new file:   Programming Basics/7_Advanced_Loops/13_Number_Pyramid/Number_Pyramid.cs 	new file:   Programming Basics/7_Advanced_Loops/13_Number_Pyramid/Properties/AssemblyInfo.cs 	modified:   Programming Basics/7_Advanced_Loops/7_Advanced_Loops.sln

--- Programming Basics/7_Advanced_Loops/11_Enter_Even_Number/Enter_Even_Number.cs
+++ Programming Basics/7_Advanced_Loops/11_Enter_Even_Number/Enter_Even_Number.cs
@@ -4,7 +4,31 @@ class Enter_Even_Number
 {
     static void Main()
     {
+        int n = 0;        
 
+        while (true)
+        {
+            Console.WriteLine("Enter even number: ");
+
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number is not even.");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+
+        Console.WriteLine("Even number entered: {0}", n);
     }
 }
 
---
+++ Programming Basics/7_Advanced_Loops/13_Number_Pyramid/Number_Pyramid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Number_Pyramid
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int num = 1;
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(num + " ");
+                num++;
+
+                if (num > n)
+                {
+                    return;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
