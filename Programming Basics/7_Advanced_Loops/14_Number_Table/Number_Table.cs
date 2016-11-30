e35b15620685f2ffe752323969e15aa51b5174fd
Deyan <dublinstand@gmail.com>
2016-11-30 14:36:30 +0000

    new file:   Programming Basics/7_Advanced_Loops/14_Number_Table/14_Number_Table.csproj 	new file:   Programming Basics/7_Advanced_Loops/14_Number_Table/App.config 	new file:   Programming Basics/7_Advanced_Loops/14_Number_Table/Number_Table.cs 	new file:   Programming Basics/7_Advanced_Loops/14_Number_Table/Properties/AssemblyInfo.cs 	new file:   Programming Basics/7_Advanced_Loops/15_StupidPasswordGenerator/15_StupidPasswordGenerator.csproj 	new file:   Programming Basics/7_Advanced_Loops/15_StupidPasswordGenerator/App.config 	new file:   Programming Basics/7_Advanced_Loops/15_StupidPasswordGenerator/Properties/AssemblyInfo.cs 	new file:   Programming Basics/7_Advanced_Loops/15_StupidPasswordGenerator/StupidPasswordGenerator.cs 	new file:   Programming Basics/7_Advanced_Loops/16_MagicNumbers/16_MagicNumbers.csproj 	new file:   Programming Basics/7_Advanced_Loops/16_MagicNumbers/App.config 	new file:   Programming Basics/7_Advanced_Loops/16_MagicNumbers/MagicNumbers.cs 	new file:   Programming Basics/7_Advanced_Loops/16_MagicNumbers/Properties/AssemblyInfo.cs 	new file:   Programming Basics/7_Advanced_Loops/17_SpecialNumbers/17_SpecialNumbers.csproj 	new file:   Programming Basics/7_Advanced_Loops/17_SpecialNumbers/App.config 	new file:   Programming Basics/7_Advanced_Loops/17_SpecialNumbers/Properties/AssemblyInfo.cs 	new file:   Programming Basics/7_Advanced_Loops/17_SpecialNumbers/SpecialNumbers.cs 	modified:   Programming Basics/7_Advanced_Loops/7_Advanced_Loops.sln

---
+++ Programming Basics/7_Advanced_Loops/14_Number_Table/Number_Table.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Number_Table
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int num;
+
+        for (int col = 0; col < n; col++)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                num = row + col + 1;
+
+                if (num > n )
+                {
+                    num = 2 * n - num;
+                    Console.Write(num + " ");
+                }
+                else
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
+
---
+++ Programming Basics/7_Advanced_Loops/15_StupidPasswordGenerator/StupidPasswordGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class StupidPasswordGenerator
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int l = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                for (char k = 'a'; k < 'a' + l; k++)
+                {
+                    for (char w = 'a'; w < 'a' + l; w++)
+                    {
+                        for (int q = Math.Max(i, j) + 1; q <= n; q++)
+                        {
+                            Console.Write("{0}{1}{2}{3}{4} ", i, j, k, w, q);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
